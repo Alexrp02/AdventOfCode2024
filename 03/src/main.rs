dfb9ce494150a84b9bfe7284c596c09718f96eef
AlexRP <aleeexrp02@gmail.com>
2024-12-08 01:43:08 +0100

    (03) Third day completed

--- 03/src/main.rs
+++ 03/src/main.rs
@@ -12,15 +12,22 @@ fn get_text_from_filename(filename: &String) -> String {
     let text: &mut String = &mut String::from("");
     let _ = BufReader::new(file).read_to_string(text);
 
-    text.to_string()
+    text.trim().to_string()
 }
 
 fn main() {
     let input_text = get_text_from_filename(&String::from("input.txt"));
+    println!("{input_text}");
+
+    let separating_re = Regex::new(r"(?s)\A(.*?)(?:don't\(\)|$)|do\(\)(.*?)(?:don't\(\)|$)").expect("Error creating the separating re");
+
+    let new_input:String = separating_re.find_iter(input_text.as_str()).map(|m| String::from(m.as_str())).collect::<Vec<String>>().join("");
+
+    println!("New input is {new_input}");
 
     let regex = Regex::new(r"mul\(\d+,\d+\)").expect("Error creating the regex");
 
-    let array_of_match = get_array_of_match_from_regex(input_text, regex);
+    let array_of_match = get_array_of_match_from_regex(new_input, regex);
 
     process_matches_of_mul(array_of_match);
 }
